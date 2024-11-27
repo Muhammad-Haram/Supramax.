19995e8fb4b34fd0f3f46c3f4275116a86f479b8
Muhammad-Haram <haramh643@gmail.com>
2024-11-27 15:14:51 +0500

    commit

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array, required: true },
